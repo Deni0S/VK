b3f58392ca4369a89042df11cd78d820ddf7ecfe
Denis Barinov <DgamFX@mail.ru>
2020-04-21 00:41:30 +0300

    Перевод сетевых запросов в фоновый поток GCD.

--- VK/Service/VKService.swift
+++ VK/Service/VKService.swift
@@ -57,6 +57,12 @@ class VKService {
     }
     // Дополнительный параметр запроса
     var additionalParametr = ""
+    let photoQueue = DispatchQueue(label: "Photo Queue", qos: .utility)
+    let friendQueue = DispatchQueue(label: "Friend Queue", qos: .utility)
+    let groupQueue = DispatchQueue(label: "Group Queue", qos: .utility)
+    let groupSearchQueue = DispatchQueue(label: "GroupSearch Queue", qos: .utility)
+    let newsQueue = DispatchQueue(label: "News Queue", qos: .utility)
+    
     // Получить URL
     func getURLpath(for method: VKServiceMethod) -> String {
         var param = ""
@@ -69,27 +75,33 @@ class VKService {
     
     // Фотографии
     func getPhoto(id: String, complition: ((Error?) -> Void)? = nil) {
-        // Добавим дополнительный параметр id пользователя
-        additionalParametr = "owner_id=\(id)&"
-        // Создать URL по частям
-        let urlPath = self.getURLpath(for: .getPhoto)
-        // Статический метод получения ответа Alamofire
-        AF.request(urlPath).responseData {responce in
-            // Обработать ошибки
-            if let error = responce.error {
-                complition?(error)
-                print(error)
-            } else {
-                // SwiftyJSON cериализация
-                if let json = try? JSON(data: responce.value!) {
-                    print("\nJson Фотографии:\n", json)
-                    // SwiftyJSON Парсинг
-                    let photo = json["response"]["items"].arrayValue.map { Photo(json: $0) }
-                    print("\nФотографии:\n\(photo)")
-                    // Сохранить в Realm
-                    self.savePhotoData(photo)
-                    // Передадим данные через замыкание
-                    complition?(nil)
+        // Перейти в последовательную, фоновую очередь для фотографий
+        photoQueue.async {
+            // Добавим дополнительный параметр id пользователя
+            self.additionalParametr = "owner_id=\(id)&"
+            // Создать URL по частям
+            let urlPath = self.getURLpath(for: .getPhoto)
+            // Статический метод получения ответа Alamofire
+            AF.request(urlPath).responseData {responce in
+                // Обработать ошибки
+                if let error = responce.error {
+                    complition?(error)
+                    print(error)
+                } else {
+                    // SwiftyJSON cериализация
+                    if let json = try? JSON(data: responce.value!) {
+                        print("\nJson Фотографии:\n", json)
+                        // SwiftyJSON Парсинг
+                        let photo = json["response"]["items"].arrayValue.map { Photo(json: $0) }
+                        print("\nФотографии:\n\(photo)")
+                        // Сохранить в Realm
+                        self.savePhotoData(photo)
+                        // Вызвать кусочек кода в главном потоке асинхронно
+                        DispatchQueue.main.async {
+                            // Передадим данные через замыкание
+                            complition?(nil)
+                        }
+                    }
                 }
             }
         }
@@ -97,39 +109,48 @@ class VKService {
     
     // Сохранить фото в Realm
     func savePhotoData(_ vk: [Photo]) {
-        let realm = try! Realm()
-        let oldPhoto = realm.objects(Photo.self)
-        do {
-            try realm.write {
-                realm.delete(oldPhoto)
-                realm.add(vk, update: .all)
+        // Выполнить в одиночку, чтобы приложение не вылетало при удалении Realm
+        photoQueue.async(flags: .barrier) {
+            let realm = try! Realm()
+            let oldPhoto = realm.objects(Photo.self)
+            do {
+                try realm.write {
+                    realm.delete(oldPhoto)
+                    realm.add(vk, update: .all)
+                }
+            } catch {
+                print(error)
             }
-        } catch {
-            print(error)
         }
     }
     
     // Друзья
     func getFriend(complition: ((Error?) -> Void)? = nil) {
-        // Создать URL по частям
-        let urlPath = self.getURLpath(for: .getFriend)
-        // Статический метод полчения ответа Alamofire
-        AF.request(urlPath).responseData {responce in
-            // Обработать ошибки
-            if let error = responce.error {
-                complition?(error)
-                print(error)
-            } else {
-                // SwiftyJSON cериализация
-                if let json = try? JSON(data: responce.value!) {
-                    print("\nJson Друзья:\n", json)
-                    // SwiftyJSON Парсинг
-                    let friend = json["response"]["items"].arrayValue.map { User(json: $0) }
-                    print("\nДрузья:\n\(friend)")
-                    // Сохранить в Realm
-                    self.saveFriendData(friend)
-                    // Передадим данные через замыкание
-                    complition?(nil)
+        // Перейти в последовательную, фоновую очередь для друзей
+        friendQueue.async {
+            // Создать URL по частям
+            let urlPath = self.getURLpath(for: .getFriend)
+            // Статический метод полчения ответа Alamofire
+            AF.request(urlPath).responseData {responce in
+                // Обработать ошибки
+                if let error = responce.error {
+                    complition?(error)
+                    print(error)
+                } else {
+                    // SwiftyJSON cериализация
+                    if let json = try? JSON(data: responce.value!) {
+                        print("\nJson Друзья:\n", json)
+                        // SwiftyJSON Парсинг
+                        let friend = json["response"]["items"].arrayValue.map { User(json: $0) }
+                        print("\nДрузья:\n\(friend)")
+                        // Сохранить в Realm
+                        self.saveFriendData(friend)
+                        // Вызвать кусочек кода в главном потоке асинхронно
+                        DispatchQueue.main.async {
+                            // Передадим данные через замыкание
+                            complition?(nil)
+                        }
+                    }
                 }
             }
         }
@@ -137,39 +158,47 @@ class VKService {
     
     // Сохранить друзей в Realm
     func saveFriendData(_ vk: [User]) {
-        let realm = try! Realm()
-        let oldFriend = realm.objects(User.self)
-        do {
-            try realm.write {
-                realm.delete(oldFriend)
-                realm.add(vk, update: .all)
+        // Выполнить в одиночку, чтобы приложение не вылетало при удалении Realm
+//        friendQueue.async(flags: .barrier) {
+            let realm = try! Realm()
+            let oldFriend = realm.objects(User.self)
+            do {
+                try realm.write {
+                    realm.delete(oldFriend)
+                    realm.add(vk, update: .all)
+                }
+            } catch {
+                print(error)
             }
-            
-        } catch {
-            print(error)
-        }
+//        }
     }
     
     // Группы
     func getGroup(complition: ((Error?) -> Void)? = nil) {
-        // Создать URL по частям
-        let urlPath = self.getURLpath(for: .getGroup)
-        // Статический метод получения ответа Alamofire
-        AF.request(urlPath).responseData {responce in
-            // Обработать ошибки
-            if let error = responce.error {
-                complition?(error)
-                print(error)
-            } else {
-                // SwiftyJSON cериализация
-                if let json = try? JSON(data: responce.value!) {
-                    print("\nJson Группы:\n", json)
-                    let group = json["response"]["items"].arrayValue.map { Group(json: $0) }
-                    print("\nГруппы:\n\(group)")
-                    // Сохранить в Realm
-                    self.saveGroupData(group)
-                    // Передадим данные через замыкание
-                    complition?(nil)
+        // Перейти в последовательную, фоновую очередь для групп
+        groupQueue.async {
+            // Создать URL по частям
+            let urlPath = self.getURLpath(for: .getGroup)
+            // Статический метод получения ответа Alamofire
+            AF.request(urlPath).responseData {responce in
+                // Обработать ошибки
+                if let error = responce.error {
+                    complition?(error)
+                    print(error)
+                } else {
+                    // SwiftyJSON cериализация
+                    if let json = try? JSON(data: responce.value!) {
+                        print("\nJson Группы:\n", json)
+                        let group = json["response"]["items"].arrayValue.map { Group(json: $0) }
+                        print("\nГруппы:\n\(group)")
+                        // Сохранить в Realm
+                        self.saveGroupData(group)
+                        // Вызвать кусочек кода в главном потоке асинхронно
+                        DispatchQueue.main.async {
+                            // Передадим данные через замыкание
+                            complition?(nil)
+                        }
+                    }
                 }
             }
         }
@@ -177,41 +206,50 @@ class VKService {
     
     // Сохранить группы в Realm
     func saveGroupData(_ vk: [Group]) {
-        let realm = try! Realm()
-        let oldGroup = realm.objects(Group.self)
-        do {
-            try realm.write {
-                realm.delete(oldGroup)
-                realm.add(vk, update: .all)
+        // Выполнить в одиночку, чтобы приложение не вылетало при удалении Realm
+        groupQueue.async(flags: .barrier) {
+            let realm = try! Realm()
+            let oldGroup = realm.objects(Group.self)
+            do {
+                try realm.write {
+                    realm.delete(oldGroup)
+                    realm.add(vk, update: .all)
+                }
+            } catch {
+                print(error)
             }
-        } catch {
-            print(error)
         }
     }
     
     // Поиск групп
     func getGroupSearch(search: String, complition: (([Group]?, (Error?)) -> Void)? = nil) {
-        // Добавить дополнительный параметр для поискового запроса
-        additionalParametr = "q=\(search)&"
-        // Создать URL по частям
-        let urlPath = self.getURLpath(for: .getGroupSearch)
-        // Статический метод получения ответа Alamofire
-        AF.request(urlPath).responseData {responce in
-            // Обработать ошибки
-            if let error = responce.error {
-                complition?(nil, error)
-                print(error)
-            } else {
-                // SwiftyJSON cериализация
-                if let json = try? JSON(data: responce.value!) {
-                    print("\nJson Поиск групп:\n", json)
-                    // SwiftyJSON Парсинг
-                    let groupSearch = json["response"]["items"].arrayValue.map { Group(json: $0) }
-                    print("\nПоиск групп:\n\(groupSearch)")
-                    // Сохранить группы в Firestore
+        // Перейти в последовательную, фоновую очередь для поиска по группам
+        groupSearchQueue.async {
+            // Добавить дополнительный параметр для поискового запроса
+            self.additionalParametr = "q=\(search)&"
+            // Создать URL по частям
+            let urlPath = self.getURLpath(for: .getGroupSearch)
+            // Статический метод получения ответа Alamofire
+            AF.request(urlPath).responseData {responce in
+                // Обработать ошибки
+                if let error = responce.error {
+                    complition?(nil, error)
+                    print(error)
+                } else {
+                    // SwiftyJSON cериализация
+                    if let json = try? JSON(data: responce.value!) {
+                        print("\nJson Поиск групп:\n", json)
+                        // SwiftyJSON Парсинг
+                        let groupSearch = json["response"]["items"].arrayValue.map { Group(json: $0) }
+                        print("\nПоиск групп:\n\(groupSearch)")
+                        // Сохранить группы в Firestore
 //                    self.saveGroupSearchFirestore(groupSearch)
-                    // Передадим данные через замыкание
-                    complition?(groupSearch, nil)
+                        // Вызвать кусочек кода в главном потоке асинхронно
+                        DispatchQueue.main.async {
+                            // Передадим данные через замыкание
+                            complition?(groupSearch, nil)
+                        }
+                    }
                 }
             }
         }
@@ -219,48 +257,54 @@ class VKService {
     
     /* // Сохранить группы в Firestore
     func saveGroupSearchFirestore(_ vk: [Group]) {
-        let db = Firestore.firestore()
-        db.collection("id\(Session.instance.userid)").document("GroupSearch").setData(vk
-            .map { $0.toFirestore() }
-            .reduce([:]) { $0.merging($1) { (current, _) in current } }
-        ) { error in
-            if let error = error {
-                print(error.localizedDescription)
-            } else { print("data saved") }
+        // Перейти в последовательную, фоновую очередь для поиска по группам
+        groupSearchQueue.async(flags: .barrier) {
+            let db = Firestore.firestore()
+            db.collection("id\(Session.instance.userid)").document("GroupSearch").setData(vk
+                .map { $0.toFirestore() }
+                .reduce([:]) { $0.merging($1) { (current, _) in current } }
+            ) { error in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else { print("data saved") }
+            }
         }
     }
      */
 
     // Новости
     func getNews(complition: ((Error?) -> Void)? = nil) {
-        // Создать URL по частям
-        let urlPath = self.getURLpath(for: .getNews)
-        // Статический метод получения ответа Alamofire
-        AF.request(urlPath).responseData {responce in
-            // Обработать ошибки
-            if let error = responce.error {
-                // Вызвать кусочек кода в главном потоке асинхронно
-                DispatchQueue.main.async {
-                    complition?(error)
-                }
-            } else {
-                // SwiftyJSON Cериализация
-                if let json = try? JSON(data: responce.value!) {
-//                        debugPrint(json)
-                    // SwiftyJSON Парсинг
-                    let newsProfiles = json["response"]["profiles"].arrayValue.map { NewsProfiles(json: $0) }
-                    let newsGroups = json["response"]["groups"].arrayValue.map { NewsGroups(json: $0) }
-//                        debugPrint(newsProfiles, newsGroups)
-                    // Сохранить источник  новостей в базу Realm
-                    self.saveSourceNews (newsProfiles, newsGroups)
-                    let news = json["response"]["items"].arrayValue.map { News(json: $0) }
-//                        debugPrint(news)
-                    // Сохранить новости вместе с исночниками в Realm
-                    self.saveNewsData(news)
+        // Перейти в последовательную, фоновую очередь для новостей
+        newsQueue.async {
+            // Создать URL по частям
+            let urlPath = self.getURLpath(for: .getNews)
+            // Статический метод получения ответа Alamofire
+            AF.request(urlPath).responseData {responce in
+                // Обработать ошибки
+                if let error = responce.error {
                     // Вызвать кусочек кода в главном потоке асинхронно
                     DispatchQueue.main.async {
-                        // Передать данные через замыкание
-                        complition?(nil)
+                        complition?(error)
+                    }
+                } else {
+                    // SwiftyJSON Cериализация
+                    if let json = try? JSON(data: responce.value!) {
+//                        debugPrint(json)
+                        // SwiftyJSON Парсинг
+                        let newsProfiles = json["response"]["profiles"].arrayValue.map { NewsProfiles(json: $0) }
+                        let newsGroups = json["response"]["groups"].arrayValue.map { NewsGroups(json: $0) }
+//                        debugPrint(newsProfiles, newsGroups)
+                        // Сохранить источник  новостей в базу Realm
+                        self.saveSourceNews (newsProfiles, newsGroups)
+                        let news = json["response"]["items"].arrayValue.map { News(json: $0) }
+//                        debugPrint(news)
+                        // Сохранить новости вместе с исночниками в Realm
+                        self.saveNewsData(news)
+                        // Вызвать кусочек кода в главном потоке асинхронно
+                        DispatchQueue.main.async {
+                            // Передать данные через замыкание
+                            complition?(nil)
+                        }
                     }
                 }
             }
@@ -286,16 +330,18 @@ class VKService {
     
     // Сохранить в Realm новости вместе с источниками
     func saveNewsData(_ ns: [News]) {
-        let realm = try! Realm()
-        let oldNews = realm.objects(News.self)
-        do {
-            try realm.write {
-                realm.delete(oldNews)
-                realm.add(ns, update: .all)
+        // Выполнить в одиночку, чтобы приложение не вылетало при удалении Realm
+        newsQueue.async(flags: .barrier) {
+            let realm = try! Realm()
+            let oldNews = realm.objects(News.self)
+            do {
+                try realm.write {
+                    realm.delete(oldNews)
+                    realm.add(ns, update: .all)
+                }
+            } catch {
+                print(error)
             }
-        } catch {
-            print(error)
         }
-        
     }
 }
